787fae7358ddd0c5fa26d0a052aa9940566aef4d
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-10-22 21:22:10 +0530

    route handler

---
+++ Desktop/NextJS/Route-handler/route/src/app/comments/data.js
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id:1,
+        text:"this is line 1",
+    },
+    {
+        id:2,
+        text:"this is line 2",
+    },
+    {
+        id:3,
+        text:"this is line 3",
+    },
+];
